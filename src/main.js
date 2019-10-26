60fc117891d743161a2be96cbb5cad8169b6e06e
Carlos Rojas <carlos.rojas@localhost>
2019-10-26 17:52:32 -0500

    custom element

--- src/main.js
+++ src/main.js
@@ -1,8 +1,20 @@
 import Vue from 'vue'
 import App from './App.vue'
+import vueCustomElement from 'vue-custom-element';
 
+Vue.config.ignoredElements = [
+  'app-element'
+];
+
+Vue.use(vueCustomElement);
+
+Vue.customElement('app-element', App, {});
+
+
+/*
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+*/
